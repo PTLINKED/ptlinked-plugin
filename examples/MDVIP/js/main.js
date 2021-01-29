c07846f3c889a9362bb9efb22ce4f0626ef19c1d
Mike Frank <mfrank@ptlinked.com>
2021-01-29 13:40:34 -0500

    Initial MDVIP Example
    The initial commit of the MDVIP example project

---
+++ examples/MDVIP/js/main.js
@@ -0,0 +1,39 @@
+
+var ptlinked_app = null ;
+$(document).ready( function(){
+
+	console.log( "... Initialize Plugin" ) ;
+	ptlinked_app = $("#ptlinked--application_container").ptlinkedLibrary({
+		api_key: 'e7ae8a7a7be9491c063674a79a657de9',
+		api_root_url: 'https://api-mdvip.ptlinked.com',
+		app_root_url: 'https://mdvip-plugin.ptlinked.com/test',		
+		save_favorites: true,
+		secure_messaging: true,		
+		training_mode: false,
+		debug_mode: true,
+		user_type: 'physician',
+		user_uid: 3,		
+		style_sheet: '',
+        header_element_class: 'site-header',
+        viewer_header_element_class: 'viewer--header',
+        viewer_thumb_scroller_class: 'viewer--header_bar',
+        dialog_box_type: 'jquery', // jquery or hook
+		onInit: function(){
+			console.log( "Plugin has been initialized" ) ;
+		},
+		onSendProgram: function( data ) {
+			console.log( "Send Program" ) ;
+			console.dir( data ) ;
+		},
+		onPrintProgram: function( data ) {
+			console.log( "Print Program" ) ;
+			console.dir( data ) ;	
+		},
+		onShowDialog: function( data ) {
+			console.log( "SHOW DIALOG BOX" ) ;
+			console.log( "Title: " + data["title"] ) ;
+			console.log( "Content: " + data["content"] ) ;
+		}
+	}) ;
+
+});
